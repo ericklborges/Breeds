de53e8191a11a9bd2205842904a36be50eaedc82
Erick Borges <erick.lozano.borges@accenture.com>
2020-08-10 21:43:36 -0300

    Add Identifier enum

---
+++ Breeds/Sources/Identifier.swift
@@ -0,0 +1,18 @@
+//
+//  Identifier.swift
+//  Breeds
+//
+//  Created by erick.lozano.borges on 10/08/20.
+//  Copyright © 2020 borges.erick. All rights reserved.
+//
+
+import Foundation
+
+enum Identifier {
+    enum Cell {
+        static let breedCell = "BreedCell"
+    }
+    enum Segue {
+        static let goToBreedDetail = "GoToBreedDetail"
+    }
+}
