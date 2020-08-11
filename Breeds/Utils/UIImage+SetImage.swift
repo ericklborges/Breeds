2c75f26bcd98ed4f070d04235ff5742f52dfb735
Erick Borges <erick.lozano.borges@accenture.com>
2020-08-10 21:28:27 -0300

    Add UIImageView KingFisher extension

---
+++ Breeds/Utils/UIImage+SetImage.swift
@@ -0,0 +1,16 @@
+//
+//  UIImage+SetImage.swift
+//  Breeds
+//
+//  Created by erick.lozano.borges on 09/08/20.
+//  Copyright © 2020 borges.erick. All rights reserved.
+//
+
+import Kingfisher
+import UIKit
+
+extension UIImageView {
+    func setImage(url: URL?) {
+        self.kf.setImage(with: url)
+    }
+}
