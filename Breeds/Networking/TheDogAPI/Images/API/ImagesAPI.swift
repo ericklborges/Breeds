a6b6ce0f7955a438d71027b4e2c7dcff3fd6af8e
Erick Borges <erick.lozano.borges@accenture.com>
2020-08-15 16:21:45 -0300

    Update ImagesAPI to accept pagination

--- Breeds/Networking/TheDogAPI/Images/API/ImagesAPI.swift
+++ Breeds/Networking/TheDogAPI/Images/API/ImagesAPI.swift
@@ -16,12 +16,14 @@ class ImagesAPI {
         self.manager = manager
     }
     
-    func fetchImages(breedId: Int, completion: @escaping(Result<[Image], Error>) -> Void) {
+    func fetchImages(limit: Int, page: Int, completion: @escaping(Result<[Image], Error>) -> Void) {
         let url = TheDogAPISources.baseUrl.appendingPathComponent("/v1/images/search")
         let headers = TheDogAPISources.authHeader
         
-        let parameters = [
-            "breed_id": breedId
+        let parameters: [String: Any] = [
+            "order": "Asc",
+            "limit": limit,
+            "page": page
         ]
         
         manager.request(url: url, method: .get, parameters: parameters, headers: headers, completion: completion)
