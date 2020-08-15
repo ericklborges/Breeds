7ba102d3e3054463e060d765d27e9c59f725b394
Erick Borges <erick.lozano.borges@accenture.com>
2020-08-15 17:51:14 -0300

    Add BreedDetailViewController and View

---
+++ Breeds/BreedDetail/Controller/BreedDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BreedDetailViewController.swift
+//  Breeds
+//
+//  Created by erick.lozano.borges on 15/08/20.
+//  Copyright © 2020 borges.erick. All rights reserved.
+//
+
+import UIKit
+
+class BreedDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Breeds/BreedDetail/View/BreedDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  BreedDetailView.swift
+//  Breeds
+//
+//  Created by erick.lozano.borges on 15/08/20.
+//  Copyright © 2020 borges.erick. All rights reserved.
+//
+
+import UIKit
+
+class BreedDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
